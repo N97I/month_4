e227663bc73db6a5e709ef84e0513f5543dcf650
N97I <ildan3216@gmail.com>
2025-04-17 02:02:14 +0600

    hw3

--- posts/models.py
+++ posts/models.py
@@ -11,12 +11,29 @@ from django.db import models
 
 # one object - SELECT * FROM post WHERE id=1 ==> Post.objects.get(id=1)
 # """
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+
+    def __str__(self):
+        return f'{self.name}'
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f'{self.name}'
+
 class Post(models.Model):
+    image = models.ImageField(null=True, blank=True)
     title = models.CharField(max_length=356)
     content = models.CharField(max_length=856)
     rate = models.IntegerField(default=0)
-    created_at = models.DateField(auto_now_add=True, null=True)
-    updated_at = models.DateField(auto_now=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    tags = models.ManyToManyField(Tag)
 
 
 
